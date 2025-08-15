34d7e618863ebb044de2f5417eca5d67157c3731
MichaelChangeChen <michaelexgt@gmail.com>
2025-08-15 11:14:44 +0800

    add GetScore PostDaVinciCodeName PostDaVinciCodeSave api

--- DaVinciCode.py
+++ DaVinciCode.py
@@ -1,14 +1,14 @@
+from fastapi.responses import JSONResponse
 from fastapi import FastAPI, Request, Depends
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import JSONResponse
 from fastapi.exceptions import RequestValidationError
 
-from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import get_db
-from daVinciCode_models import DaVinciCodeResult
 import daVinciCode_schema as schemas
+from daVinciCode_models import DaVinciCodeResult
 
 import random
 
@@ -16,9 +16,9 @@ app = FastAPI()
 app.add_middleware(
 	CORSMiddleware,
 	allow_origins=["*"],
-	allow_credentials=True,
 	allow_methods=["*"],
 	allow_headers=["*"],
+	allow_credentials=True,
 )
 
 # 初始化遊戲
@@ -33,7 +33,7 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
 	return JSONResponse(
 		status_code=422,
 		content={
-			"message": "請輸入正確的整數數字！",
+			"message": "錯誤",
 			"errors": exc.errors(),
 		},
 	)
@@ -47,15 +47,28 @@ async def global_exception_handler(request, exc):
 	)
 # ===============================
 
-@app.get('/resetGame', response_model=schemas.ResetGameResponse)
-async def reset(db: AsyncSession = Depends(get_db)):
-	global guess_time, min_num, max_num, guess_num
-	result = await db.execute(
-		select(DaVinciCodeResult).order_by(DaVinciCodeResult.guess_time.asc()).limit(1)
-	)
-	best = result.scalar_one_or_none()
-	best_time = schemas.DaVinciCodeResultBase.from_orm(best) if best else None
+@app.post('/checkName')
+async def check_name(
+	data: schemas.PostDaVinciCodeName,
+	db: AsyncSession = Depends(get_db)
+):
+	print("1. 開始 resetGame")
+	result = await db.execute( select(DaVinciCodeResult).where(DaVinciCodeResult.name == data.name) )
+	existing = result.scalar_one_or_none()
+	if existing:
+		return {
+			'message': 'This name is already in use.',
+			'statusCode': 2
+		}
+	else:
+		return {
+			'message': '你名字很棒耶',
+			'statusCode': 1
+		}
 
+@app.get('/resetGame', response_model=schemas.GetResetGame)
+async def reset():
+	global guess_time, min_num, max_num, guess_num
 	guess_num = random.randint(0, 100)
 	max_num = 100
 	min_num = 1
@@ -65,16 +78,35 @@ async def reset(db: AsyncSession = Depends(get_db)):
 		'tips': f'select number {min_num} to {max_num} .',
 		'max_num': max_num,
 		'guess_time': guess_time,
-		'best_time': best_time,
 		'statusCode': 1
 	}
 
-# 前端送的猜測資料格式
-class GuessRequest(schemas.DaVinciCodeResultCreate):
-	guess: int
+@app.get('/getScore', response_model=schemas.GetScore)
+async def reset(db: AsyncSession = Depends(get_db)):
+	print("1. 開始 resetGame")
+
+	result = await db.execute(
+		select(DaVinciCodeResult).order_by(DaVinciCodeResult.guess_time.asc()).limit(1)
+	)
+	best = result.scalar_one_or_none()
+	best_time = schemas.DaVinciCodeResultBase.from_orm(best) if best else None
+
+	score_list_result = await db.execute(
+		select(DaVinciCodeResult).order_by(DaVinciCodeResult.guess_time.asc()).limit(5)
+	)
+	score_list = score_list_result.scalars().all()
+	print("2. DB execute 完成")
+	print("3. best:", score_list_result)
+	print("4. best:", score_list)
+
+	return {
+		'best_time': best_time,
+		'score_list': score_list,
+		'statusCode': 1
+	}
 
 @app.post('/guessGame')
-def guess_game(data: GuessRequest):
+def guess_game(data: schemas.PostDaVinciCode):
 	global guess_time, min_num, max_num, guess_num
 	print(data.guess)
 	user_guess = data.guess
@@ -115,11 +147,12 @@ def guess_game(data: GuessRequest):
 		}
 
 # [4] 遊戲結束後新增紀錄
-@app.post("/addRecord", response_model=schemas.DaVinciCodeResultResponse)
-async def add_record(record: schemas.DaVinciCodeResultCreate, db: AsyncSession = Depends(get_db)):
+@app.post("/saveRecord")
+async def add_record(data: schemas.PostDaVinciCodeSave, db: AsyncSession = Depends(get_db)):
+	print("1. 開始 resetGame")
 	new_record = DaVinciCodeResult(
-		name=record.name,
-		guess_time=record.guess_time,
+		name=data.name,
+		guess_time=data.guess_time,
 	)
 	db.add(new_record)
 	await db.commit()
--- daVinciCode_schema.py
+++ daVinciCode_schema.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 from datetime import datetime
-from typing import Optional
+from typing import Optional, List
 
 
 # 進入遊戲前要給前端的完整資料（抓最少 guess_time 用）
@@ -14,27 +14,31 @@ class DaVinciCodeResultBase(BaseModel):
 		from_attributes = True  # 讓 Pydantic 支援 SQLAlchemy ORM 物件
 
 
-class ResetGameResponse(BaseModel):
-    message: str
-    tips: str
-    max_num: int
-    guess_time: int
-    best_time: Optional[DaVinciCodeResultBase] = None
-    statusCode: int
-
-
-# 新增紀錄用（遊戲結束時傳給後端）
-class DaVinciCodeResultCreate(BaseModel):
-	name: str
+class GetResetGame(BaseModel):
+	message: str
+	tips: str
+	max_num: int
 	guess_time: int
+	statusCode: int
+
+class GetScore(BaseModel):
+	best_time: Optional[DaVinciCodeResultBase] = None
+	score_list: List[DaVinciCodeResultBase] = []
+	statusCode: int
+
+
+# 前端送的猜測資料格式
+class PostDaVinciCode(BaseModel):
+	guess: int
 	play_date: Optional[datetime] = None  # 不給的話後端會用 default=datetime.utcnow
 
+class PostDaVinciCodeName(BaseModel):
+	name: str
+
 # 回傳用（新增後或查詢後的完整資料）
-class DaVinciCodeResultResponse(BaseModel):
-	id: str
+class PostDaVinciCodeSave(BaseModel):
 	name: str
 	guess_time: int
-	play_date: datetime
 
 	class Config:
 		from_attributes = True    #讓你在回傳 ORM 物件時，不需要手動轉成字典
